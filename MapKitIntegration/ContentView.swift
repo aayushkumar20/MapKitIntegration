4786ae1534ac701d3582604215bf0642c32d5e82
aayushkumar20 <aayushkumarjsr16@protonmail.com>
2023-10-25 09:37:39 +0530

    Initial Commit

---
+++ MapKitIntegration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKitIntegration
+//
+//  Created by Aayush kumar on 25/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitIntegration/MapKitIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitIntegrationApp.swift
+//  MapKitIntegration
+//
+//  Created by Aayush kumar on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MapKitIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
